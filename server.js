70b3e52934afeaa5cb6a4802867cc2ce91ac5ae7
Helder <helder.virissio@gmail.com>
2026-04-28 11:27:07 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,37 +3,29 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
-
-// Configurações
 app.use(cors());
 app.use(express.json());
 
-// Link do seu MongoDB
 const mongoURI = "mongodb+srv://gabi:1234567890@ativ-mongo-dev.qspekqm.mongodb.net/tarefas?appName=ativ-mongo-dev";
 
-// Conexão
 mongoose.connect(mongoURI)
   .then(() => console.log("✅ MongoDB Conectado"))
   .catch(err => console.log("❌ Erro de conexão:", err));
 
-// Modelo
-const Tarefa = mongoose.model('Tarefa', { 
-  titulo: { type: String, required: true } 
-});
+const Tarefa = mongoose.model('Tarefa', { titulo: String });
 
-// Rotas (API)
 app.get('/tarefas', async (req, res) => {
-  const tarefas = await Tarefa.find();
-  res.json(tarefas);
+  const t = await Tarefa.find();
+  res.json(t);
 });
 
 app.post('/tarefas', async (req, res) => {
-  const novaTarefa = await Tarefa.create(req.body);
-  res.json(novaTarefa);
+  const t = await Tarefa.create(req.body);
+  res.json(t);
 });
 
-// IMPORTANTE: O Render usa a porta que ele mesmo define ou a 10000
+// O segredo para o Render é esta linha abaixo:
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
-  console.log(`🚀 Servidor rodando na porta ${PORT}`);
+  console.log(`🚀 Servidor ON na porta ${PORT}`);
 });
