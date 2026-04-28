669ddb8caf5e9cadf9ed84be8e45b7fa9ef89e9d
Helder <helder.virissio@gmail.com>
2026-04-28 11:22:01 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,1 @@
-mongodb+srv://gabi:1234567890@ativ-mongo-dev.qspekqm.mongodb.net/?appName=ativ-mongo-dev
+const mongoURI = "mongodb+srv://gabi:1234567890@ativ-mongo-dev.qspekqm.mongodb.net/tarefas?appName=ativ-mongo-dev";
